7512673763e5e909b15b673f9648111436c78b80
augustearth <augustearth@gmail.com>
2020-03-10 18:59:17 -0700

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # carnival-vue
+Demonstration Carnival Vue application.
---
+++ carnival-workspace.js
@@ -0,0 +1,9 @@
+//import Vue from 'vue'
+// import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+var app = new Vue({
+  el: '#app',
+  data: {
+    message: 'Hello Vue!'
+  }
+})
