cd4007770fba8f65654a58097d16ece5138dad44
David Birtwell <augustearth@gmail.com>
2020-03-11 20:17:45 -0700

    bootstrap-vue

--- carnival-workspace.js
+++ carnival-workspace.js
@@ -1,9 +1,21 @@
-//import Vue from 'vue'
+// import { Vue } from "./node_modules/vue/dist/vue.js";
 // import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 
 var app = new Vue({
   el: '#app',
   data: {
-    message: 'Hello Vue!'
-  }
+    name: '',
+    message: 'WHAT THE FUCK!',
+    info: null
+  },
+  computed: {
+    showAlert() {
+      return this.name.length > 4 ? true : false
+    }
+  },
+  mounted () {
+    axios
+      .get('http://localhost/cm/person/list/')
+      .then(response => (this.info = response))
+  }  
 })
