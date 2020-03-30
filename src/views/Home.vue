97ce32b54ec0ef37d5c1f3c521a6427da2e6e4e2
augustearth <augustearth@gmail.com>
2020-03-30 13:46:48 -0700

    remove bootstrap. add tailwind.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,35 +1,29 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/carnival-squash.jpg" width="25%" height="25%" />
-    <b-container>
-      <b-jumbotron header="CarnivalVue" lead="Carnival Graph Model Validation">
-        <p>For more information visit our website</p>
-        <b-btn variant="primary" href="https://github.com/augustearth/carnival-vue">More Info</b-btn>
-      </b-jumbotron>
+    <p>{{ name }}</p>
 
-      <b-form-group
-        horizontal
-        :label-cols="4"
-        description="Let us know your name."
-        label="Enter your name"
-      >
-        <b-form-input v-model.trim="name"></b-form-input>
-      </b-form-group>
+    <img
+      alt="Vue logo"
+      src="../assets/carnival-squash.jpg"
+      width="25%"
+      height="25%"
+    />
 
-      <b-alert variant="success" :show="showAlert">Hello {{ name }}</b-alert>
-      <p>{{ name }}</p>
-    </b-container>
-    <HelloWorld msg="Welcome to Carnival-Vue" />
+    <p class="text-blue-400 hover:bg-blue-400 hover:text-white">
+      Going to TailwindCSS this all up... no more Bootstrap.
+    </p>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+//import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "Home",
-  components: { HelloWorld },
+  components: {
+    /*HelloWorld*/
+  },
   data() {
     return {
       name,
